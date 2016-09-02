2d780ed6b4485696fc6d1064804ccc73b5c052c7
Arnelle Balane <arnellebalane@gmail.com>
2016-09-02 20:24:35 +0800

    prepend slash to service worker path

--- starwarspwa/static/starwarspwa/javascripts/sw-register.js
+++ starwarspwa/static/starwarspwa/javascripts/sw-register.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
         console.info('Service worker successfully registered.');
     }).catch(function(error) {
         console.error('Failed to register service worker', error);
